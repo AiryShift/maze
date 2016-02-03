a7b6d824e11efc085d0f6896908544d4b58a113c
AiryShift <religionofaperturelabs@gmail.com>
2016-02-03 23:48:22 +1100

    Create maze.py: file that makes a maze

---
+++ maze.py
@@ -0,0 +1,12 @@
+import random
+
+def make(w, h):
+    """
+    Returns a boolean array representing a maze
+    True for path, False for walls
+    """
+    maze = [[False for _ in range(w)] for _ in range(h)]
+    print(maze)
+
+if __name__ == '__main__':
+    pass
