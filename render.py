d37193de1b647d4ff3fc2b25943a37dd3f0e4cce
AiryShift <religionofaperturelabs@gmail.com>
2016-03-01 15:13:46 +1100

    render.py: no magic numbers!

--- render.py
+++ render.py
@@ -3,6 +3,8 @@ import numpy as np
 from maze import make_maze
 
 SIZE_OF_SQUARE = 15
+BLACK = 0
+WHITE = 255
 
 
 def render(w, h, out='a.png'):
@@ -11,7 +13,11 @@ def render(w, h, out='a.png'):
     bitmap = np.zeros((h * SIZE_OF_SQUARE, w * SIZE_OF_SQUARE), dtype='uint8')
     for x, row in enumerate(maze):
         for y, cell in enumerate(row):
-            colour = 255 if cell else 0
+            colour = WHITE if cell else BLACK
+            if x == 0 and y == 1 or x == h - 1 and y == w - 2:
+                colour = WHITE
+            elif x == 0 or y == 0 or x == h - 1 or y == w - 1:
+                colour = BLACK
             bX, bY = (i * SIZE_OF_SQUARE for i in (x, y))
             bitmap[bX:bX + SIZE_OF_SQUARE, bY:bY + SIZE_OF_SQUARE] = colour
     rendered_image = Image.fromarray(bitmap)
